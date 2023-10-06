c154bc74d9149e506994243d952aba32020e77b7
Russ Painter <russp@geekymonkey.com>
2023-10-06 21:06:53 +0100

    Add monsters with navigation

---
+++ Monster.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+
+@export var SPEED = 2.0
+const ACCEL = 10.0
+
+@onready var nav: NavigationAgent3D = $NavigationAgent3D
+@onready var player: Node3D = $"../Player"
+
+var age: float = 0
+
+func _physics_process(delta: float) -> void:
+	age += delta
+	DoNav(delta)
+
+func DoNav(delta: float):
+	if (age < 1.0):
+		return
+	nav.target_position = player.global_position
+
+	var direction = Vector3()
+	direction = nav.get_next_path_position() - global_position
+	direction.y = 0
+	direction = direction.normalized()
+
+	velocity = direction * SPEED
+
+	move_and_slide()
