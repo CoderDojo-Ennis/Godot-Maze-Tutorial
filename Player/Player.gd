3dac269287ee6fcc76f4ea79ddcb4261931e7abf
Russ Painter <russp@geekymonkey.com>
2023-10-28 07:26:41 +0100

    1st person movement with mouse + WASD

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,24 +5,56 @@ extends CharacterBody3D
 @export var Health: float = 100.0
 @export var FirstPerson: bool = false
 @export var JumpVelocity: float = 4.5
-@export var Speed: float = 15.0
+@export var Speed: float = 12.0
 @export var SpinSpeed: float = 2.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var mouse_sensitivity: float = 0.002
+var pitch: float = 0
+var yaw: float = 0
+var rotation_default: Vector3
+
 
 @onready var camera_1st_person: Camera3D = $Camera1stPerson
 @onready var camera_3rd_person: Camera3D = $Camera3rdPerson
 
 
-func _ready() -> void:
-	Events.ExitHit.connect(exit_was_hit)
 
+func _ready() -> void:
+	rotation_default = rotation_degrees
+	Events.ExitHit.connect(exit_was_hit)
+	set_view_mode()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+
+func set_view_mode():
 	# Choose the camera for the input mode
 	if FirstPerson:
 		camera_1st_person.make_current()
+
+		# Level the camera when switching to 1st person
+		rotation.x = rotation_default.x
 	else:
 		camera_3rd_person.make_current()
+
+
+func _input(event: InputEvent) -> void:
+	# C button changes the camera view
+	if (event.is_action_pressed("Camera Change")):
+		FirstPerson = !FirstPerson
+		set_view_mode()
+
+	# ESC key to exit the game
+	elif (event.is_action_pressed("Exit")):
+		get_tree().quit()
+
+	elif event is InputEventMouseMotion:
+		yaw -= event.relative.x * mouse_sensitivity
+		pitch -= event.relative.y * mouse_sensitivity
+		pitch = clamp(pitch, 0, PI/2)
+		rotation_degrees.x = rad_to_deg(pitch)
+		rotation_degrees.y = rad_to_deg(yaw)
 
 
 func _physics_process(delta: float) -> void:
@@ -36,27 +68,20 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 
-	if FirstPerson:
-		rotate_y(-input_dir.x * SpinSpeed * delta)
-		var direction := (transform.basis.z).normalized()
-		var vely = velocity.y
-		velocity = direction * input_dir.y * Speed
-		velocity.y = vely
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * Speed
+		velocity.z = direction.z * Speed
 	else:
-		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		if direction:
-			velocity.x = direction.x * Speed
-			velocity.z = direction.z * Speed
-		else:
-			velocity.x = move_toward(velocity.x, 0, Speed)
-			velocity.z = move_toward(velocity.z, 0, Speed)
+		velocity.x = move_toward(velocity.x, 0, Speed)
+		velocity.z = move_toward(velocity.z, 0, Speed)
 
 	# Add the gravity.
 	if not on_floor:
 		velocity.y -= gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept"):
+	if Input.is_action_just_pressed("Jump"):
 		if on_floor:
 			velocity.y = JumpVelocity
 
